2ae758b900cbe62692207780f2ee10f97a635446
Keith Frazier <keithers98@gmail.com>
2021-03-28 16:05:53 -0700

    added timer functionality

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -1,25 +1,64 @@
 import React, { useState } from "react";
 import classNames from "../utils/class-names";
 import useInterval from "../utils/useInterval";
-import { minutesToDuration } from "../utils/duration";
+import { minutesToDuration, secondsToDuration } from "../utils/duration";
+import './Pomodoro.css'
 
 function Pomodoro() {
   // Timer starts out paused
   const [isTimerRunning, setIsTimerRunning] = useState(false);
   const [focusTime, setFocusTime] = useState(25);
   const [breakTime, setBreakTime] = useState(5);
+  const [displayTimer, setDisplayTimer ] = useState("hide")
+  const [timeSurpassed, setTimeSurpassed] = useState(0)
+  const [totalTime, setTotalTime] = useState(focusTime * 60)
+  const [secondsLeft, setSecondsLeft ]= useState(0)
+  const [timeLeft, setTimeLeft] = useState(0)
+
+
+  //let timeSurpassed = 0
+  //let totalTime = focusTime * 60 
+  //let secondsLeft = 0 
+  //let timeLeft = 0 
 
   useInterval(
     () => {
+      setTimeSurpassed((prevState) => prevState = prevState + 1)
+      setSecondsLeft((prevState) => prevState = totalTime - timeSurpassed)
+      //secondsLeft = totalTime - timeSurpassed
+      setTimeLeft((prevState) => prevState = secondsToDuration(secondsLeft))
+      //timeLeft = secondsToDuration(secondsLeft)
+      console.log()
+      console.log(timeLeft)
+      console.log(secondsLeft)
+      console.log(timeSurpassed)
+      console.log(totalTime)
       // ToDo: Implement what should happen when the timer is running
     },
     isTimerRunning ? 1000 : null
   );
+  
+  function handleStop(){
+    setSecondsLeft((prevState) => prevState = 0)
+    setTimeLeft((prevState) => prevState = 0)
+    setTimeSurpassed((prevState) => prevState = 0)
+    setIsTimerRunning((prevState) => !prevState);
+    setDisplayTimer((prevState) => prevState = "hide")
+    console.log()
+    console.log(timeLeft)
+    console.log(secondsLeft)
+    console.log(timeSurpassed)
+    console.log(totalTime)
+  }
 
   function playPause() {
+    setTotalTime(focusTime * 60)
+    setSecondsLeft((prevState) => prevState = totalTime - timeSurpassed)
     setIsTimerRunning((prevState) => !prevState);
+    setDisplayTimer((prevState) => prevState = "show")
+    
+
   }
-  //{event:{target:{dataset:{testid}}}}
   function changeTime({
     target: {
       dataset: { testid },
@@ -133,19 +172,42 @@ function Pomodoro() {
               type="button"
               className="btn btn-secondary"
               title="Stop the session"
+              onClick = {handleStop}
             >
               <span className="oi oi-media-stop" />
             </button>
           </div>
         </div>
       </div>
-      <div className = "hide">
+      <div className = {displayTimer}>
         {/* TODO: This area should show only when a focus or break session is running or pauses */}
         <div className="row mb-2">
           <div className="col">
             {/* TODO: Update message below to include current session (Focusing or On Break) and total duration */}
-            <h2 data-testid="session-title">Focusing for 25:00 minutes</h2>
+            <h2 data-testid="session-title">Focusing for {minutesToDuration(focusTime)} minutes</h2>
             {/* TODO: Update message below to include time remaining in the current session */}
             <p className="lead" data-testid="session-sub-title">
-              25:00 remaining
-            <
+              {timeLeft} remaining
+            </p>
+          </div>
+        </div>
+        <div className="row mb-2">
+          <div className="col">
+            <div className="progress" style={{ height: "20px" }}>
+              <div
+                className="progress-bar"
+                role="progressbar"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                aria-valuenow={timeSurpassed} // TODO: Increase aria-valuenow as elapsed time increases
+                style={{ width:  `${((timeSurpassed/totalTime)*100).toString()}%` }} // TODO: Increase width % as elapsed time increases
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Pomodoro;
