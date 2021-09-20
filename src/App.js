654893e486d332dd4ff3efa6d1f557a13e74f21d
Keith Frazier <keithers98@gmail.com>
2021-09-20 12:24:55 -0700

    added styling

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Pomodoro from "./pomodoro/Pomodoro";
 function App() {
   return (
     <div className="App">
-      <header className="App-header container">
-        <h1>Pomodoro Timer</h1>
-      </header>
-      <div className="container">
-        <Pomodoro />
+      <div className="centerContainer">
+        <div className="contentContainer">
+          <header className="App-header container">
+            <h1>Pomodoro Timer</h1>
+          </header>
+          <div>
+            <Pomodoro />
+          </div>
+        </div>
       </div>
     </div>
   );
